ab9dff32a17bc8bc8cb88031dd0a66afe143fbb5
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-11-19 18:51:19 +0530

    Done

---
+++ Day 6/script.js
@@ -0,0 +1,60 @@
+// -3 
+
+// var a=-3
+// let ans=Math.abs(a)
+// console.log(ans)
+
+// 3 4 10 
+// let secAns=Math.max(3,4,10)
+// console.log(secAns)
+
+// let secAns=Math.min(3,4,10)
+// console.log(secAns)
+
+// let num=29.9999
+// let ans=Math.floor(num)
+// console.log(ans)
+
+
+// let num=29.5
+// let ans=Math.round(num)
+// console.log(ans)
+
+
+
+
+// Arrays...................................................
+// let arr=[12,17,19,67.87,54.21,87]
+// arr.push(91)
+// arr.pop()
+// let ans= arr.splice(3,2,5)
+// console.log(ans)
+// arr.unshift(20)
+// arr.shift()
+// let ans=arr.slice(3,6)
+// console.log(arr)
+// console.log(ans)
+
+
+
+
+/*
+function myFunc(theArr) {
+    theArr[0] = 54;
+  }
+  
+  const arr = [45];
+  
+  console.log(arr[0]); // 45
+  myFunc(arr);
+  console.log(arr[0]); // 54
+*/
+
+
+// Strings....................................................................
+let str="I am Boy"
+// let ans=str.slice(-3)
+// let ans=str.replace("I   am","We")
+let ans=str.split("").join(" ")
+
+console.log(ans)
