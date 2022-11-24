afd4d9c51eba21412d4229f49f0319bed8b35633
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-11-24 20:55:17 +0530

    Done

---
+++ Day 10/script.js
@@ -0,0 +1,80 @@
+/*
+https://docs.google.com/document/d/1nbLWpdd4HzpMdxSwpDYNRM9HKCwJ0Z5KeV8fQXJJJak/edit
+problem 1
+
+if the number is even and less than 15, print a
+if the number is even and more than 15, print b
+if the number is odd and less than 10, print c
+if the number is odd and more than 10, print d
+
+We need to solve this using two methods
+using nested if else
+using logical operators
+
+
+problem 2
+Result
+answer should be: "Hi, my name is Farheena"
+we need to solve this using for loop
+
+problem 3
+
+global scope and local scope
+
+
+problem 4
+
+create a variable called myName and store your name inside it.
+then, add Mr./Miss. before it and store the result in
+a new variable with the same name i.e. myName.
+
+problem 5
+
+Find sum and multiplication of  all natural numbers from 10 to 20
+
+
+
+problem 6
+
+Check whether a given year is a leap year
+ 
+create a variable and store a random year in it 
+now write code, which should return  true or false, depending whether the value in the above variable is leap year or not
+
+
+problem 7
+
+find the area of a triangle.  Lengths of its sides are 5, 6, 7
+
+
+problem 8
+
+create two variables and store numbers in them.
+Now, write code, which return true if one of the variable is 50 or if their sum is 50.
+Otherwise, you need to return false.
+Also handle edge cases
+(in real world, these two numbers will be supplied by the users.
+Now think what if they supply something which is not a number ?)
+
+
+problem 9
+
+create a variable and store a number inside it
+write program to check whether a given variable is between 100 and 200
+write program to check whether a given variable is between 100 and 200 or 400 and 500
+handle edge cases
+
+
+problem 10
+
+you have 3 numbers.
+write a program which can find the largest number
+
+
+problem 11
+
+create two variables and assign numbers to them
+now write a code which returns true if one of them is 8 or their sum or difference is 8. Otherwise it should return false
+*/
+
+
