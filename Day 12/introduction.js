c746457fc5b963f13ec6df2d9f6354bcf493f1f7
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-11-27 21:02:35 +0530

    Done

--- Day 12/introduction.js
+++ Day 12/introduction.js
@@ -1,33 +1,37 @@
-/*JavaScript (JS) :-is the world’s most popular lightweight, interpreted compiled programming language. 
-It is also known as a scripting language for web pages. It can be used for Client-side as well as 
-Server-side developments.*/
+/*JavaScript (JS) :-is the world’s most popular lightweight, interpreted 
+compiled programming language. It is also known as a scripting language 
+for web pages. It can be used for Client-side as well as Server-side 
+developments.*/
 
 
-/*Datatypes in JavaScript:-There are majorly two types of languages. First, one is Statically typed 
-language where each variable and expression type is already known at compile time. Once a variable is
-declared to be of a certain data type, it cannot hold values of other data types.
-Example: C, C++, Java. 
-Other, Dynamically typed languages: These languages can receive different data types over time. 
+/*Datatypes in JavaScript:-There are majorly two types of languages. 
+First, one is Statically typed language where each variable and 
+expression type is already known at compile time. Once a variable is
+declared to be of a certain data type, it cannot hold values of other 
+data types.Example: C, C++, Java. 
+Other, Dynamically typed languages: These languages can receive
+different data types over time. 
 For example- Ruby, Python, JavaScript, etc.
+
 
 Premitive- Numbers,String,Boolean,Null,Undefined,Symbol,bigint
 Non Premitive- Array,Object
 */
 
 
-/*Variables in JavaScript:-Variables in JavaScript are containers that hold reusable data.
- It is the basic unit of storage in a program.  */
+/*Variables in JavaScript:-Variables in JavaScript are containers that
+ hold reusable data.It is the basic unit of storage in a program.  */
 
 
 
-/*Loop in JavaScript:- loops are used to repeat a block of code. For example, if you want 
-to show a message 100 times, then you can use a loop. It's just a simple example;
- you can achieve much more with loops.*/
+/*Loop in JavaScript:- loops are used to repeat a block of code.
+ For example, if you want to show a message 100 times, then you can use 
+ a loop. It's just a simple example; you can achieve much more with loops.*/
 
 
 
- /*Operator in JavaScript:- An operator performs some operation on single or multiple
-  operands (data value) and produces a result
+ /*Operator in JavaScript:- An operator performs some operation on single 
+ or multiple operands (data value) and produces a result
   1:-Arithmetic Operators
   2:-Comparison Operators
   3:-Logical Operators
@@ -40,9 +44,11 @@ to show a message 100 times, then you can use a loop. It's just a simple example
 Example- const arr=["Abhishek",12,19,7.88,"Ballia"];*/
 
 
-/*Object:- Object is a non-premitive data types that allows you to store multiple collection of data.
+/*Object:- Object is a non-premitive data types that allows you to store 
+multiple collection of data.
 Example- const obj={Name:"Abhishek",Address:"Ballia",mob:9598628521};*/
 
 
-/*DOM:-"The W3C Document Object Model (DOM) is a platform and language-neutral interface that allows
- programs and scripts to dynamically access and update the content, structure, and style of a document."*/
+/*DOM:-"The W3C Document Object Model (DOM) is a platform and language-neutral 
+interface that allows programs and scripts to dynamically access and update the 
+content, structure, and style of a document."*/
