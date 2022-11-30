f57519947ee97c31fa4145d529d1d6b2199d1013
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-11-30 18:43:37 +0530

    Done

---
+++ Day 15/Evaluation_1.js
@@ -0,0 +1,16 @@
+/*
+
+1)Introduce Yourself
+2)Explain Any two Assignment 
+3)What is Array
+4)What is Object
+5)Difference B/W let,const,var
+6)what is Loop can you write code Infinite 
+7)Difference B/W Map and Filter
+8)Why is scope is present
+9)What is scope
+10)What is syntax
+11)What is function
+12)What is ternary operator Why we using it
+
+*/
---
+++ Day 15/script.js
@@ -0,0 +1,42 @@
+const places = ['delhi', 'gurgaon', 'jaipur', 'pune']
+const morePlaces = ['kochi', 'hyderabad', 'Shimla', 'Srinagar']
+
+// IMPORTANT: solve all problems using using destructuring and rest syntax
+
+		
+// ============================================================================= //
+//1.
+// remove first element from places array and print the remaining array 
+//result = ['gurgaon', 'jaipur', 'pune']
+
+
+// ============================================================================= //
+//2.
+// insert that element at the start of the morePlaces array and print the new array
+// result =  ['delhi', 'kochi', 'hyderabad', 'Shimla', 'Srinagar']
+
+// ============================================================================= //
+// 3.
+// take out last three element from the morePlacesArray and take out first three elements from the places array 
+// and print the combined array
+//result =  ['hyderabad', 'Shimla', 'Srinagar', 'delhi', 'gurgaon', 'jaipur',]
+
+
+
+const myData = {
+    name: "Rahul",
+    age: 20,
+    gender: "male",
+    address: {
+      pinCode: 123455,
+      city:"delhi"
+    },
+    likes:["music", "movies"]
+  }
+  
+// 4.
+// destructure and store pincode into a variable named myPincode
+  
+// 5.
+// destructure and store second like into a vaiable named myLike
+  
