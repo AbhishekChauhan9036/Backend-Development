65727e9a8b3d4adb1fef8497cac40dc5316b561f
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-03 19:56:49 +0530

    Done

--- Day 18/logicBuilding.js
+++ Day 18/logicBuilding.js
@@ -1,39 +1,45 @@
-Week 3
-
+// Week 3
 
 const customerDetails = {
-    "name": "Ashok",
-    "daysSinceLastPurchase": 25,
-    "daysSinceLastLogin": 15,
-    "totalOrders": 19,
-    "membershipDuration": .5
- }
+  name: "Ashok",
+  daysSinceLastPurchase: 25,
+  daysSinceLastLogin: 15,
+  totalOrders: 19,
+  membershipDuration: 0.5,
+};
 
- let discountOffer = 0
+let discountOffer = 0;
 
- // 4) If a user’s total order is greater than 20, assign them a discount offer of 25%
-if(customerDetails.totalOrders > 20) {
-    discountOffer = 25
+// 4) If a user’s total order is greater than 20, assign them a discount offer of 25%
+if (customerDetails.totalOrders > 20) {
+  discountOffer = 25;
 }
 
 // 3) If a customer has not logged in for the last 30 days and also not purchased something, assign a discount offer of 20%
-if(customerDetails.daysSinceLastLogin > 30 && customerDetails.daysSinceLastPurchase > 30) {
-    discountOffer = 20
+if (
+  customerDetails.daysSinceLastLogin > 30 &&
+  customerDetails.daysSinceLastPurchase > 30
+) {
+  discountOffer = 20;
 }
 
 // 2) if a customer has logged-in in the last 30 days but hasn’t purchased anything in these 30 days, assign a discount offer of 18%
-if(customerDetails.daysSinceLastLogin <= 30 && customerDetails.daysSinceLastPurchase > 30) {
-    discountOffer = 18
+if (
+  customerDetails.daysSinceLastLogin <= 30 &&
+  customerDetails.daysSinceLastPurchase > 30
+) {
+  discountOffer = 18;
 }
 
- // 1) If membership is more than a year old, assign them a discount offer of 5%
+// 1) If membership is more than a year old, assign them a discount offer of 5%
 
-if(customerDetails.membershipDuration > 1) {
-    discountOffer = 5
+if (customerDetails.membershipDuration > 1) {
+  discountOffer = 5;
 }
 
-
-console.log(`The discount offer for ${customerDetails.name} is evaluated to be ${discountOffer}`) 
+console.log(
+  `The discount offer for ${customerDetails.name} is evaluated to be ${discountOffer}`
+);
 
 /*
 You are given a customer details and you have been told to give a discount percentage to the customer based on the following details
@@ -56,11 +62,10 @@ Customer details
 }
 */
 
-
-
-
+/*
 
 Book mentioned in the session - Atomic Habits by James Clear
 
 A tweet by Professor Feynman's tribute Twitter account (https://twitter.com/proffeynman)
 When one teaches, two learn.
+*/
