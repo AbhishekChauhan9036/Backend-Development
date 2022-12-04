21f1de2d6718a8f16e9d026ff62073de02874f0a
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-04 20:11:16 +0530

    Done

---
+++ Day 19/twoSum.js
@@ -0,0 +1,14 @@
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
+
+
+let nums = [2, 7, 11, 15],target = 9;
+console.log(twoSum(nums, target));
