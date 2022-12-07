17b77b7265614fa1deba6da2a20c7566e4752b7e
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-07 12:43:35 +0530

    NodeJs Day 2

---
+++ Day 20/leetCode.js
@@ -0,0 +1,45 @@
+/*Solve this three problem on Leetcode problems till lunch. 
+
+1. Two Sum
+2. Palindrome Number 
+3. Remove Duplicates from sorted Array*/
+
+/* 1.Two Sum Solution.....................................
+Question Link:- https://leetcode.com/problems/two-sum/description/ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) return [i, j];
+    }
+  }
+};
+
+
+
+/*2. Palindrome Number Solution.....................................
+Question Link:- https://leetcode.com/problems/palindrome-number/  */
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  let temp = x,
+    rTl = 0;
+  while (x > 0) {
+    rTl = rTl * 10 + (x % 10);
+    x = parseInt(x / 10);
+  }
+  return rTl == temp;
+};
+
+
+
+/*3. Remove Duplicates from sorted Array Solution.....................................
+Question Link:- https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/  */
+var removeDuplicates = function(nums) {
+    if(nums.length==0)return 0;
+    let i=0;
+    for(let j=1;j<nums.length;j++){
+        if(nums[j]!=nums[i]){
+            nums[++i]=nums[j];
+        }
+    }
+    return i+1;
+};
