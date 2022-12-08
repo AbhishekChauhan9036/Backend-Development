88438900add71648956a849a03f17fdfd8e8bc8b
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-08 20:44:34 +0530

    NodeJs Day 4

---
+++ Day 22/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./routes/route.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', route);
+
+app.listen(3000, function() {
+    console.log('Express app running on port ' + 3000)
+});
+
+
+// A || B
---
+++ Day 22/src/routes/route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const intro = require('./introduction')
+const employee = require('./employee')
+const _ = require('underscore')
+
+router.get('/test-me', function (req, res) {
+    console.log("email from introduction module", intro.myEmail)
+    intro.myFunction('Sabiha')
+    console.log("email from employee module", employee.myEmail)
+
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let result = _.first(days, 4)
+    console.log(`Result from underscore function is ${result}`)
+
+
+    res.send('any dummy text')
+});
+
+
+router.get('/test-you', function(req, res){
+    console.log("I am here")
+    res.send("very important text")
+})
+
+
+module.exports = router;
